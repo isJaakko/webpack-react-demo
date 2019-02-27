cb2d450f356fd6fdea5ca49ff756dbadd1dd54f7
jie jun. yin <yinjunjie@xwfintech.net>
2019-02-27 18:35:14 +0800

    可编辑表格

--- src/component/editableCell/index.js
+++ src/component/editableCell/index.js
@@ -1,53 +1,16 @@
 import React, { Component } from 'react';
 import { Input, Form } from 'antd';
 
-const FormItem = Form.Item;
+const { Item } = Form;
 const EditableContext = React.createContext();
 const { Provider, Consumer } = EditableContext;
-const EditableRow = ({ form, index, ...props }) => (
-    <Provider value={form}>
-        <tr {...props} />
-    </Provider>
-);
 
 const EditableFormRow = Form.create()(EditableRow);
 
 export default class EditableCell extends React.Component {
     render() {
-        const {
-            editing,
-            dataIndex,
-            title,
-            inputType,
-            record,
-            index,
-            ...restProps
-        } = this.props;
-
         return (
             <Consumer>
-                {
-                    (form) => {
-                        const { getFieldDecorator } = form;
-                        return (
-                            <td {...restProps}>
-                                {editing ? (
-                                    <FormItem style={{ margin: 0 }}>
-                                        {getFieldDecorator(dataIndex, {
-                                            rules: [{
-                                                required: true,
-                                                message: `Please Input ${title}!`,
-                                            }],
-                                            initialValue: record[dataIndex],
-                                        })(
-                                            <Input />
-                                        )}
-                                    </FormItem>
-                                ) : restProps.children}
-                            </td>
-                        );
-                    }
-                }
             </Consumer>
         );
     }
--- src/view/setting/comment/index.js
+++ src/view/setting/comment/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { Table, Input, Button, Popconfirm, Form, Divider } from 'antd';
-import EditableCell from 'component/editableCell';
-import { EditableFormRow, Consumer } from 'component/editableCell';
+import { Table, Col, Input } from 'antd';
 
+const { Column } = Table;
 const data = [];
 for (let i = 0; i < 5; i++) {
     data.push({
@@ -13,151 +12,58 @@ for (let i = 0; i < 5; i++) {
     });
 }
 
-export default class CommentTemplate extends Component {
+export default class EditableTable extends Component {
     constructor(props) {
         super(props);
         this.state = {
             data,
             editingKey: ''
         };
-
-        this.handleAdd = this.handleAdd.bind(this);
-
-        this.columns = [
-            {
-                title: 'name',
-                dataIndex: 'name',
-                editable: true,
-            },
-            {
-                title: 'operation',
-                width: '25%',
-                dataIndex: 'operation',
-                render: (text, record) => {
-                    const editable = this.isEditing(record);
-                    return (
-                        <div>
-                            {editable ? (
-                                <span>
-                                    <Consumer>
-                                        {form => (
-                                            <a
-                                                href="javascript:;"
-                                                onClick={() => this.save(form, record.key)}
-                                                style={{ marginRight: 8 }}
-                                            >
-                                                Save
-                                            </a>
-                                        )}
-                                    </Consumer>
-                                    <Popconfirm
-                                        title="Sure to cancel?"
-                                        onConfirm={() => this.cancel(record.key)}
-                                    >
-                                        <a>Cancel</a>
-                                    </Popconfirm>
-                                </span>
-                            ) : (<span>
-                                <a
-                                    onClick={() => this.edit(record.key)}
-                                    style={{ marginRight: 8 }}>
-                                    Edit
-                                </a>
-                                <Popconfirm
-                                    title="Sure to delete?"
-                                    onConfirm={() => this.cancel(record.key)}
-                                >
-                                    <a>delete</a>
-                                </Popconfirm>
-                            </span>)}
-                        </div>
-                    );
-                },
-            },
-        ];
     }
 
-    isEditing(record) {
-        return record.key === this.state.editingKey;
-    }
-
-    cancel() {
-        this.setState({ editingKey: '' });
-    };
-
-    save(form, key) {
-        form.validateFields((error, row) => {
-            if (error) {
-                return;
-            }
-            const newData = [...this.state.data];
-            const index = newData.findIndex(item => key === item.key);
-            if (index > -1) {
-                const item = newData[index];
-                newData.splice(index, 1, {
-                    ...item,
-                    ...row,
-                });
-                this.setState({ data: newData, editingKey: '' });
-            } else {
-                newData.push(row);
-                this.setState({ data: newData, editingKey: '' });
-            }
-        });
-    }
-
-    edit(key) {
-        this.setState({ editingKey: key });
-    }
-
-    handleAdd() {
-        const { count, data } = this.state;
-        const newData = {
-            key: (data.length - 1).toString(),
-            name: `Edrward ${data.length - 1}`,
-            age: 32,
-            address: `London Park no. ${data.length - 1}`,
-        };
-        this.setState({
-            data: [...data, newData],
-            count: count + 1,
-        });
+    isEditing(key) {
+        console.log(key, this.editingKey);
+        return key === this.editingKey;
     }
 
     render() {
-        const components = {
-            body: {
-                row: EditableFormRow,
-                cell: EditableCell,
-            },
-        };
-
-        const columns = this.columns.map((col) => {
-            if (!col.editable) {
-                return col;
-            }
-            return {
-                ...col,
-                onCell: record => ({
-                    record,
-                    dataIndex: col.dataIndex,
-                    title: col.title,
-                    editing: this.isEditing(record),
-                }),
-            };
-        });
-
         return (<div>
-            <Button type="primary" onClick={this.handleAdd} style={{ marginBottom: 16 }}>
-                add a row
-            </Button>
             <Table
-                components={components}
                 bordered
                 dataSource={this.state.data}
-                columns={columns}
                 rowClassName="editable-row"
-            />
+            >
+                <Column
+                    title="名字"
+                    dataIndex="name"
+                    render={(text, record, index) => {
+                        const editing = this.isEditing(record.key);
+                        // console.log('isEditing', editing);
+                        return (editing ? <Input value={text} />
+                            : <span>text</span>)
+                    }}
+                />
+                <Column
+                    title="年龄"
+                    dataIndex="age"
+                />
+                <Column
+                    title="操作"
+                    dataIndex="option"
+                    render={(text, record, index) => {
+                        return (<a
+                            href="javascript:;"
+                            onClick={() => {
+                                this.setState({
+                                    editingKey: record.key
+                                }, () => { console.log(this.state) })
+                            }}
+                        >
+                            edit
+                        </a>)
+                    }}
+                />
+            </Table>
         </div>
         );
     }
